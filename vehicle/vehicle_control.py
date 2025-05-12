92ff85e0a60902fcf7a6fbb179eaf2526023e566
IlyaBOT <isarbeu@gmail.com>
2025-05-12 13:10:02 +0300

    Fix blur & contrast

--- vehicle/vehicle_control.py
+++ vehicle/vehicle_control.py
@@ -139,16 +139,13 @@ class BinaryDataHandler:
                 # ret, frame = video.read()
                 image = remove_small_particles(images)
                 
-                result_line = cv2.blur(result_line,(6,6))
+                gauss_result_line = cv2.GaussianBlur(result_line, (3, 3), 0)
+                median_result_line = cv2.medianBlur(gauss_result_line, 3)
                 
-                # Apply hue change (wrap around for values outside 0-179)
-                result_line[..., 0] = (result_line[..., 0] + 6) % 180
+                alpha = 2.0  # например, 2.0 — сильный контраст
+                beta = 0     # можно добавить яркость
 
-                # Apply saturation and value changes with clipping (0-255)
-                result_line[..., 1] = np.clip(result_line[..., 1] + 16, 0, 255)
-                result_line[..., 2] = np.clip(result_line[..., 2] + 3, 0, 255)
-
-                result_line = cv2.cvtColor(result_line, cv2.COLOR_HSV2BGR)
+                result_line = cv2.convertScaleAbs(median_result_line, alpha=alpha, beta=beta)
 
                 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
                 _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
