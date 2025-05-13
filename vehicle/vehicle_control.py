98004f006ffb3c02c73cbb4672afef400c623770
IlyaBOT <isarbeu@gmail.com>
2025-05-13 17:35:12 +0300

    Remove comments

--- vehicle/vehicle_control.py
+++ vehicle/vehicle_control.py
@@ -23,54 +23,7 @@ class BinaryDataHandler:
 
         with open(filename, 'wb') as f:
             f.write(image_data)
-
-    # пример получения скриншотов с камер
-    # def example_1(self):
-    #     # скриншот с камеры 1
-    #     # посылаем на клиент сообщение, с какой камеры сделать скриншот (возможные варианты: camera1, camera2, camera3, camera4, camera5, camera6)
-    #     self.connection.send_data("camera1")
-
-    #     # получаем скриншот с камеры 1. Код ниже не будет выполняться, пока не придет скриншот с камеры 1
-    #     image_data = self.connection.receive_data()
-
-    #     self.save_image(image_data)
-
-    #     # скриншот с камеры 2
-    #     self.connection.send_data("camera2")
-
-    #     image_data = self.connection.receive_data()
-
-    #     self.save_image(image_data)
-
-    #     # скриншот с камеры 3
-    #     self.connection.send_data("camera3")
-
-    #     image_data = self.connection.receive_data()
-
-    #     self.save_image(image_data)
-
-    #     # скриншот с камеры 4
-    #     self.connection.send_data("camera4")
-
-    #     image_data = self.connection.receive_data()
-
-    #     self.save_image(image_data)
-
-    #     # скриншот с камеры 5
-    #     self.connection.send_data("camera5")
-
-    #     image_data = self.connection.receive_data()
-
-    #     self.save_image(image_data)
-
-    #     # скриншот с камеры 6
-    #     self.connection.send_data("camera6")
-
-    #     image_data = self.connection.receive_data()
-
-    #     self.save_image(image_data)
-
-    # пример управления роботом последовательным запуском команд управления
+    
     def example_2(self):
         # первый параметр - мощность вращения левых колес в процентах, второй параметр - мощность вращения правых колес.
         # если значение положительное, то колесо вращается по часовой стрелке, если отрицательно, то против часовой стрелки.
@@ -225,90 +178,6 @@ class BinaryDataHandler:
         self.vehicle.setMotorPower(0, 0)
         time.sleep(0)
 
-
-    # пример получения скриншотов и управления роботом последовательным запуском команд управления
-    # def example_3(self):
-    #     # скриншот с камеры 1
-    #     self.connection.send_data("camera1")
-
-    #     image_data = self.connection.receive_data()
-
-    #     self.save_image(image_data)
-
-    #     self.vehicle.setMotorPower(100, 100)
-    #     time.sleep(1)
-
-    #     # скриншот с камеры 2
-    #     self.connection.send_data("camera2")
-
-    #     image_data = self.connection.receive_data()
-
-    #     self.save_image(image_data)
-
-    #     self.vehicle.setMotorPower(-100, 100)
-    #     time.sleep(1)
-
-    #     # скриншот с камеры 3
-    #     self.connection.send_data("camera3")
-
-    #     image_data = self.connection.receive_data()
-
-    #     self.save_image(image_data)
-
-    #     self.vehicle.setMotorPower(60, 60)
-    #     time.sleep(3)
-
-    #     # скриншот с камеры 4
-    #     self.connection.send_data("camera4")
-
-    #     image_data = self.connection.receive_data()
-
-    #     self.save_image(image_data)
-
-    #     self.vehicle.setMotorPower(-100, 100)
-    #     time.sleep(3)
-
-    #     # скриншот с камеры 5
-    #     self.connection.send_data("camera5")
-
-    #     image_data = self.connection.receive_data()
-
-    #     self.save_image(image_data)
-
-    #     self.vehicle.setMotorPower(50, 50)
-    #     time.sleep(1)
-
-    #     # скриншот с камеры 6
-    #     self.connection.send_data("camera6")
-
-    #     image_data = self.connection.receive_data()
-
-    #     self.save_image(image_data)
-
-    #     self.vehicle.setMotorPower(-100, 80)
-    #     time.sleep(1.5)
-
-    #     self.vehicle.setMotorPower(70, 70)
-    #     time.sleep(4)
-
-    # пример использования setMotorPower и rotate
-    # def example_4(self):
-    #     self.vehicle.rotate(90)
-    #     time.sleep(1)
-
-    #     self.vehicle.rotate(-90)
-    #     time.sleep(1)
-
-    #     self.vehicle.setMotorPower(100, 100)
-    #     time.sleep(1)
-
-    #     self.vehicle.rotate(-90)
-    #     time.sleep(1)
-
-    #     self.vehicle.setMotorPower(0, 0)
-
-    #     self.vehicle.rotate(180)
-    #     time.sleep(1)
 
 async def control_vehicle(vehicle: Vehicle, connection: SocketConnection):
     data_handler = BinaryDataHandler(vehicle, connection)
